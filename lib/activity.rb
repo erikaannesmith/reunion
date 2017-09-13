e7a43fac2ca8c7864548cafe7b7cf80837e367d1
Erika Smith <erikaannesmith@gmail.com>
2017-09-13 10:03:32 -0600

    Commit for reunion

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,33 @@
+require 'pry'
 class Activity
+
+  attr_reader :name_of_activity, :participants
+
+  def initialize(name_of_activity)
+    @name_of_activity = name_of_activity
+    @participants = {}
+  end
+
+  def add_participant(name, amount_paid)
+    @participants[name] = amount_paid.to_i
+  end
+
+  def total_cost
+    @participants.values.reduce(:+)
+  end
+
+  def split_cost
+    amount_of_participants = @participants.count
+    total_cost / amount_of_participants
+  end
+
+  def balance(name)
+    if split_cost > @participants[name]
+      puts "x"
+    elsif split_cost < @participants[name]
+      puts "y"
+    end
+  end
+
 end
+# binding.pry
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,45 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    activity = Activity.new("rafting")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_a_name
+    activity = Activity.new("rafting")
+
+    assert_equal "rafting", activity.name_of_activity
+  end
+
+  def test_total_cost
+    activity = Activity.new("rafting")
+    activity.add_participant("Erika", "25")
+    activity.add_participant("Katy", "25")
+    activity.add_participant("Emily", "25")
+
+    assert_equal 75, activity.total_cost
+  end
+
+  def test_split_cost
+    activity = Activity.new("rafting")
+    activity.add_participant("Erika", "25")
+    activity.add_participant("Katy", "25")
+    activity.add_participant("Emily", "25")
+
+    assert_equal 25, activity.split_cost
+  end
+
+  def test_find_out_owed_or_owes
+    activity = Activity.new("rafting")
+    activity.add_participant("Erika", "15")
+    activity.add_participant("Katy", "25")
+    activity.add_participant("Emily", "35")
+
+    assert_equal 10, activity.balance("Erika")
+    assert_equal -10, activity.balance("Emily")
+  end
+
 end
